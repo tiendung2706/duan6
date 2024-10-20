8fab70f92f3eb15a9b47851f84889103b2749d23
Nguyễn Ngọc Tiến Dũng <92341248+tiendung2706@users.noreply.github.com>
2024-10-20 21:08:43 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * NukeViet Content Management System
+ * @version 4.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2024 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+
+if (!defined('NV_MAINFILE')) {
+    exit('Stop!!!');
+}
+
+$db_config['dbhost'] = '127.0.0.1';
+$db_config['dbport'] = '';
+$db_config['dbname'] = 'vgemm11jtp_tancuong';
+$db_config['dbsystem'] = 'vgemm11jtp_tancuong';
+$db_config['dbuname'] = 'vgemm11jtp_tancuong';
+$db_config['dbpass'] = 'vgemm11jtp_tancuong';
+$db_config['dbtype'] = 'mysql';
+$db_config['collation'] = 'utf8mb4_unicode_ci';
+$db_config['charset'] = 'utf8mb4';
+$db_config['persistent'] = false;
+$db_config['prefix'] = 'nv4';
+
+$global_config['site_domain'] = '';
+$global_config['name_show'] = 0;
+$global_config['idsite'] = 0;
+$global_config['sitekey'] = '8c8e2fa89288ea53b944d59a8d04d8a7';// Do not change sitekey!
+$global_config['hashprefix'] = '{SSHA512}';
+$global_config['cached'] = 'files';
+$global_config['session_handler'] = 'files';
+$global_config['extension_setup'] = 3; // 0: No, 1: Upload, 2: NukeViet Store, 3: Upload + NukeViet Store
+// Readmore: https://wiki.nukeviet.vn/nukeviet4:advanced_setting:file_config
